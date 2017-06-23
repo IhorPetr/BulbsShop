422cbe4e1ad9a686a9aaaf943d631af517737720
IhorPetr <vep2014@ukr.net>
2017-06-23 19:39:42 +0300

    Add Nlog logging

--- BulbsShop/Controllers/ValuesController.cs
+++ BulbsShop/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,10 +13,17 @@ namespace BulbsShop.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
         // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            _logger.LogInformation("Index page says hello");
             return new string[] { "value1", "value2" };
         }
 
--- BulbsShop/Startup.cs
+++ BulbsShop/Startup.cs
@@ -8,22 +8,35 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using NLog.Web;
+using NLog;
+using System.IO;
 
 namespace BulbsShop
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            env.ConfigureNLog("nlog.config");
+        }
+
         public IConfigurationRoot  config { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddNLog();
+            app.AddNLogWeb();
+            LogManager.Configuration.Variables["configDir"] = Path.Combine(env.ContentRootPath,"logs");
             app.UseMvc();
         }
     }
