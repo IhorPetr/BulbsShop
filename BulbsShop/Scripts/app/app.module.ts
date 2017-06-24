c0c7e8c6e40d1c46f57fd81e4a11ce9202523d9b
IhorPetr <vep2014@ukr.net>
2017-06-24 17:55:53 +0300

    Add Bower, Setting up typescript compiler

---
+++ BulbsShop/Scripts/app/app.module.ts
@@ -0,0 +1,18 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpModule } from "@angular/http";
+import "rxjs/Rx";
+import { AppComponent } from "./app.component";
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule { }
