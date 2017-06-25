7824099a43b1068c8519216815a4d43a93545bc1
IhorPetr <vep2014@ukr.net>
2017-06-25 12:55:50 +0300

    Change version Angular 2

--- BulbsShop/Scripts/app/app.module.ts
+++ BulbsShop/Scripts/app/app.module.ts
@@ -1,18 +1,13 @@
-﻿import { NgModule } from "@angular/core";
+﻿///<reference path="../../typings/globals/core-js/index.d.ts"/>
+import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { HttpModule } from "@angular/http";
 import "rxjs/Rx";
 import { AppComponent } from "./app.component";
 @NgModule({
-    // directives, components, and pipes
-    declarations: [AppComponent
-    ],
-    // modules
+    declarations: [AppComponent],
     imports: [
         BrowserModule,
-        HttpModule
     ],
-    // providers
     providers: [
     ],
     bootstrap: [
